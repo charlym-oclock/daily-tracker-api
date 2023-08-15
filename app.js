7988474e58797175f4672a039bd49d006bbd450a
Charly Morand <charly.morand@oclock.io>
2023-08-15 20:01:54 +0200

    prepared dbConfig obj for future use

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 
+const dbConfig = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+  };
+
 
 app.get('/', (req, res) => {
     res.send("Hello World")
