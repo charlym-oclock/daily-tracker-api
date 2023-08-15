a1ae6d1a24c25979db504cb595bb9cb68d5b3b88
Charly Morand <charly.morand@oclock.io>
2023-08-15 20:00:44 +0200

    dotenv module and config added

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
-const port = 3000
+
 
 app.get('/', (req, res) => {
     res.send("Hello World")
 })
 
-app.listen(port, () => {
-    console.log(`Server is currently listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Server is currently listening on port ${process.env.PORT}`)
 })
